df8918efedbe8cc2a8c68e8ab66321f3b7f7b224
jeffpereira <jeff.matheus18@gmail.com>
2020-07-25 14:41:40 -0300

    create post and show methods with database

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,8 +1,24 @@
 const { age, date } = require('../../lib/utils');
+const db = require('../../config/db')
 
 module.exports = {
 	index(req, res) {
-		return res.render('instructors/index');
+
+		const newInstructors = new Array();
+
+		db.query(`SELECT * FROM instructors`, function(err, results) {
+			if (err) return res.send('Database Error')
+			
+		for (instructor of results.rows) {
+			const formatedServices = instructor.services.split(',')
+			newInstructors.push({
+				...instructor,
+				services: formatedServices
+			})
+		}
+			return res.render('instructors/index', { newInstructors });
+		})
+
 	},
 	create(req, res) {
 		return res.render('instructors/create');
@@ -16,16 +32,40 @@ module.exports = {
 			}
 		}
 
-		let { avatar_url, birth, name, services, gender } = req.body;
+		const query = `
+			INSERT INTO instructors (
+				name,
+				avatar_url,
+				gender,
+				services,
+				birth,
+				created_at
+			) VALUES ($1, $2, $3, $4, $5, $6)
+			RETURNING id
+		`
 
-		return;
+		const values = [
+			req.body.name,
+			req.body.avatar_url,
+			req.body.gender,
+			req.body.services,
+			date(req.body.birth).iso,
+			date(Date.now()).iso
+		]
+
+		db.query(query, values, function(err, results) {
+			if (err) return res.send('Database Error')
+			return res.redirect(`/instructors/${results.rows[0].id}`)
+		})
 	},
 	show(req, res) {
 		// req.query = is any query parameters.
 		// req.body = is the actual body of the request
 		// req.params = is route parameters. Can catch more than one
 
-		return;
+
+
+		return res.send('works');
 	},
 	edit(req, res) {
 		return;
